f101ca930ba5736cec29648af994c9b9832d9fab
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2026-07-22 23:01:49 -0300

    chore: update environment api url to new duckdns domain

--- frontend/candidato/src/environments/environment.ts
+++ frontend/candidato/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://141.148.17.246:8082/api' // Default IP for now, user will change DNS later, but actually GitHub Pages requires HTTPS. We must use a domain. Let's use the one from the PDF: https://gilsonsouzaportfolio.duckdns.org/candidato-api
+  apiUrl: 'http://atscandidato.duckdns.org/candidato-api'
 };
